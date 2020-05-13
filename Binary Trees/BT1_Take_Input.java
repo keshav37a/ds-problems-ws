d2c1a63c5696a9a445fbefd39529f2fb76e0a79e
Keshav Mathur <keshav37a@gmail.com>
2020-05-13 10:58:25 +0530

    take input and print a Binary Tree

---
+++ Binary Trees/BT1_Take_Input.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class BT1_Take_Input {
+
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        BinaryTreeNode<Integer> rootNode = takeInput(null);
+        printNodes(rootNode);
+    }
+
+    public static BinaryTreeNode<Integer> takeInput(BinaryTreeNode<Integer> rootNode){
+        if(rootNode==null){
+            System.out.println("Enter root data ");
+        }
+        int data = sc.nextInt();
+        if(data!=-1){
+            BinaryTreeNode<Integer> newNode = new BinaryTreeNode<Integer>(data);
+            System.out.println("Enter left of " + data);
+            newNode.left = takeInput(newNode);
+            System.out.println("Enter right of " + data);
+            newNode.right = takeInput(newNode);
+            return newNode;
+        }
+        return null;
+    }
+
+    public static void printNodes(BinaryTreeNode<Integer> rootNode){
+        if(rootNode==null){
+            return;
+        }
+        System.out.print(rootNode.data);
+        if(rootNode.left!=null){
+            System.out.print(" L"+rootNode.left.data);
+        }
+        if(rootNode.right!=null){
+            System.out.print(" R"+rootNode.right.data);
+        }
+        System.out.println();
+        printNodes(rootNode.left);
+        printNodes(rootNode.right);
+    }
+}
+
+class BinaryTreeNode<T>{
+    BinaryTreeNode<T> left;
+    BinaryTreeNode<T> right;
+    T data;
+
+    public BinaryTreeNode(T data){
+        this.data = data;
+    }
+}
