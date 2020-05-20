4b92c0b75f271dd869242382708f470e6c82c45a
Keshav Mathur <keshav37a@gmail.com>
2020-05-20 09:25:17 +0530

    create and insert duplicate node

---
+++ Binary_Trees/BT20_Create_Insert_Duplicate_Node.java
@@ -0,0 +1,54 @@
+/*
+
+    Create & Insert Duplicate Node
+    Send Feedback
+    Given a Binary Tree with N number of nodes, for each node create a new duplicate node, and insert that duplicate as left child of the original node.
+    Note : Root will remain same. So you just need to insert nodes in the given Binary Tree and no need to print or return anything.
+    Input format :
+    Elements in level order form (separated by space)
+    (If any node does not have left or right child, take -1 in its place)
+    Output Format :
+    Level order traversal of updated tree. (Every level in new line)
+    Constraints :
+    1 <= N <= 1000
+    Sample Input :
+    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
+    Sample Output :
+    8 
+    8 10 
+    5 10 
+    5 6 
+    2 6 7 
+    2 7
+
+*/
+
+import java.util.HashMap;
+
+public class BT20_Create_Insert_Duplicate_Node {
+    public static void main(String[] args) {
+        Utility utility = new Utility();
+        BinaryTreeNode<Integer> rootNode = utility.takeInputLevelWiseUtility();
+        rootNode = getModifiedTree(rootNode);
+        HashMap<Integer, StringBuilder> levelElementsMap = new HashMap<>();
+        utility.getTreeInFormat(rootNode, 0, levelElementsMap);
+        utility.printTreeInFormat(levelElementsMap);
+    }
+
+    public static BinaryTreeNode<Integer> getModifiedTree(BinaryTreeNode<Integer> rootNode){
+        if(rootNode==null)
+            return null;
+
+        getModifiedTree(rootNode.left);
+        getModifiedTree(rootNode.right);
+
+        int data = rootNode.data;
+        BinaryTreeNode<Integer> duplicateNode = new BinaryTreeNode(data);
+
+        BinaryTreeNode<Integer> temp = rootNode.left;
+        rootNode.left = duplicateNode;
+        duplicateNode.left = temp;
+
+        return rootNode;
+    }
+}
