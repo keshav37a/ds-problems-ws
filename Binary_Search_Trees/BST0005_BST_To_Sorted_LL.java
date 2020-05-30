58f605ebbfcf60611a583d430a38d12bc3caafe9
Keshav Mathur <keshav37a@gmail.com>
2020-05-30 23:15:05 +0530

    add desc

--- Binary_Search_Trees/BST0005_BST_To_Sorted_LL.java
+++ Binary_Search_Trees/BST0005_BST_To_Sorted_LL.java
@@ -1,3 +1,19 @@
+/*
+
+BST to LL
+Send Feedback
+Given a BST, convert it into a sorted linked list. Return the head of LL.
+Input format :
+Elements in level order form (separated by space)
+(If any node does not have left or right child, take -1 in its place)
+Output Format :
+Linked list elements (separated by space)
+Sample Input :
+8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
+Sample Output :
+2 5 6 7 8 10
+
+*/
 import java.util.LinkedList;
 
 public class BST0005_BST_To_Sorted_LL {
