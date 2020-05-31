7c2f92be20d15fbd99f8204d27e233d40578d460
Keshav Mathur <keshav37a@gmail.com>
2020-05-31 23:38:20 +0530

    add desc

--- Binary_Search_Trees/BST0011_Largest_BST.java
+++ Binary_Search_Trees/BST0011_Largest_BST.java
@@ -1,3 +1,22 @@
+/*
+
+Largest BST
+Send Feedback
+Given a Binary tree, find the largest BST subtree. That is, you need to find the BST with maximum height in the given binary tree.
+Return the height of largest BST.
+Input format :
+
+Line 1 : Elements in level order form (separated by space)
+
+(If any node does not have left or right child, take -1 in its place)
+
+Sample Input 1:
+5 10 6 2 3 -1 -1 -1 -1 -1 9 -1 -1
+Sample Output 1:
+2
+
+*/
+
 public class BST0011_Largest_BST {
     public static void main(String[] args) {
         Utility utility = new Utility();
@@ -59,10 +78,10 @@ public class BST0011_Largest_BST {
         leftSide.currentHeight = currentHeight;
         leftSide.maxHeight = maxHeight;
 
-        System.out.println("currentNode="+rootNode.data);
-        System.out.println("minValue: " + minValue + " maxValue: " + maxValue + " currentHeight: " + currentHeight + " maxHeight: " + maxHeight);
-        System.out.println();
-        
+        // System.out.println("currentNode="+rootNode.data);
+        // System.out.println("minValue: " + minValue + " maxValue: " + maxValue + " currentHeight: " + currentHeight + " maxHeight: " + maxHeight);
+        // System.out.println();
+
         return leftSide;
     }
 }
